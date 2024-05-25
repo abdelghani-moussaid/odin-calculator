21d4d11df506e620e0b5a86ad74693c4771a620f
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-05-25 13:05:36 +0100

    fix: format big int and big float

--- script.js
+++ script.js
@@ -2,82 +2,79 @@
 function operate(firstNumber, secondNumber, operator){
     switch(operator){
         case 'add':
-            return add(firstNumber, secondNumber);
+            return firstNumber + secondNumber;
         case 'subtract':
-            return subtract(firstNumber, secondNumber);
+            return firstNumber - secondNumber;
         case 'multiply':
-            return multiply(firstNumber, secondNumber);
+            return firstNumber * secondNumber;
         case 'divide':
-            return divide(firstNumber, secondNumber);
+            if(secondNumber === 0){
+                return "🤯";
+            }
+            return firstNumber / secondNumber;
         default:
             alert("Wrong operator");
             break;
     }
 }
 
-function add(firstNumber, secondNumber){
-    return firstNumber + secondNumber;
-}
-
-function subtract(firstNumber, secondNumber){
-    return firstNumber - secondNumber;
-}
-
-function multiply(firstNumber, secondNumber){
-    return firstNumber * secondNumber;
-}
-function divide(firstNumber, secondNumber){
-    if(secondNumber === 0){
-        return "🤯";
-    }
-    return firstNumber / secondNumber;
-}
-
+// stringValue is what is the input of the user and gets updated with the last result of the operations is string for better display
+// currentNumber is stringValue converted to number to calculate  
 let currentNumber = 0;
 
 function populateDisplay(stringValue){
     const input = document.querySelector("input");  
-    stringValue = stringHandler(stringValue)
+    stringValue = handleString(stringValue)
     input.value = stringValue;
     currentNumber = Number(stringValue);
 }
 
-function calculator(){
+function calculate(){
 
     let firstNumber = null;
     let secondNumber;
     let currentOperator;
+    let stringValue = ''; // display value
 
-    let stringValue = '';
     const numbers = document.querySelectorAll(".number");
     const operators = document.querySelectorAll(".operator");
     const clear = document.querySelector("#clear");
     const deleted = document.querySelector("#delete");
     const toMinus = document.querySelector("#plus-minus");
 
+    // with each number or dot clicked concatenate send it to display
     numbers.forEach((number) => {
         let getNumber = function(e) {
             if (stringValue.length <= 9) {
                 stringValue += number.textContent;
-                stringValue = stringHandler(stringValue);
-                    populateDisplay(stringValue);
+                stringValue = handleString(stringValue);
+                populateDisplay(stringValue);
             }
 
         };
         number.addEventListener("click", getNumber);
     });
+
+    // if: input then operator(+,-,*,/) are clicked 
+    // then: first number takes input and input is reset
+
+    // else: When next input and then operator are clicked and the first number is already set
+    // then: the second number takes the input, get operation of the two numbers
+    //       the second number is reset
+    
+    // if: equals is clicked; the calculator data is reset  
+
     operators.forEach((operator) => {
         operator.addEventListener("click", () => {
             if(firstNumber === null){
-                stringValue = '';
                 firstNumber = currentNumber;
+                stringValue = '';
                 operator.id === 'equals' ? firstNumber = null: currentOperator = operator.id;
             }
             else{
                 secondNumber = currentNumber;
                 firstNumber = operate(firstNumber, secondNumber, currentOperator); 
-                console.log((+firstNumber.toFixed(9)) );               
-                (typeof firstNumber === "number") ? populateDisplay(+firstNumber.toFixed(9)) : populateDisplay(firstNumber);
+                populateDisplay(firstNumber.toString());
                 operator.id === 'equals' ? firstNumber = null: currentOperator = operator.id;
                 secondNumber = null;
                 stringValue = '';
@@ -88,13 +85,12 @@ function calculator(){
         firstNumber = null;
         secondNumber = null;
         stringValue = "";
-        populateDisplay("0");
+        populateDisplay(stringValue);
     })
     deleted.addEventListener("click", ()=>{
+        // when backstroke button is clicked
+        // remove last value
         stringValue = stringValue.slice(0, -1);
-
-        if(stringValue === "")
-            stringValue = "0";
         populateDisplay(stringValue);
     })
     toMinus.addEventListener("click", ()=>{
@@ -110,11 +106,10 @@ function calculator(){
         populateDisplay(stringValue);
     })
 }
-calculator();
-
-function stringHandler(string){
-
-    // convert nulls to zero
+function handleString(string){
+    const MAX_DISPLAY_LENGTH = 11;
+    const MAX_INT_LENGTH = 9;
+    // when input is empty initially or after backstroke convert it to zero
     if(string === "")
         string = "0";
 
@@ -131,13 +126,21 @@ function stringHandler(string){
         string = string.slice(1);
     }
     
-    // if(string.length > 10){
-    //     if(Number.isInteger(+string))
-    //         string = '' + (+string).toExponential(0);
-    //     else if(string.indexOf(".") > )
-    // }
+    // if string is longer then display length : 
+    // 1.222222222222222222222222222 => slice(0,10) : 1.222222222 
+    // 55555555.52555555555555555555 => slice(0,10) : 55555555.52
+    // 55555555555555555.55555555555 => toExponential(2) : 5.55e+11
+    // 55555555555555555555555555555 => toExponential(2) : 5.55e+12
+    if(string.length > MAX_DISPLAY_LENGTH){
+        const integerLength = string.indexOf('.');
+        if(integerLength === -1 || integerLength > MAX_INT_LENGTH){
+            string = Number(string).toExponential(2);
+        } else {
+            string = Number(string.slice(0,MAX_DISPLAY_LENGTH));
+        }
+    }
     return string;
 }
 
 
-// Format big floats and big int
+calculate();
